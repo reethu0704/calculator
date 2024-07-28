726ed0196d17ac70a81d0c44e4882d203f1c68a6
reethu0704 <122683169+reethu0704@users.noreply.github.com>
2024-07-28 17:04:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let equalPressed = 0;
+let buttonInput = document.querySelectorAll(".button");
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+
+window.onload = () => {
+  input.value = "";
+};
+
+buttonInput.forEach((buttonClass) => {
+  buttonClass.addEventListener("click", () => {
+    input.value += buttonClass.value;
+  });
+});
+
+equal.addEventListener("click", () => {
+  let inputValue = input.value;
+  try {
+    let solution = eval(inputValue);
+    if (Number.isNaN(solution) || !Number.isFinite(solution)) {
+      throw new Error("Invalid mathematical expression");
+    }
+    if (Number.isInteger(solution)) {
+      input.value = solution;
+    } else {
+      input.value = solution.toFixed(4);
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+clear.addEventListener("click", () => {
+  input.value = "";
+});
+
+erase.addEventListener("click", () => {
+  input.value = input.value.substr(0, input.value.length - 1);
+});
+
+square.addEventListener("click", () => {
+    input.value = Math.pow(input.value , 2);
+  });
+
+root.addEventListener("click" ,() => {
+    input.value = Math.pow(input.value , 1/2)
+  });
+
+cuberoot.addEventListener("click" ,() => {
+    input.value = Math.pow(input.value , 1/3)
+  });
+
+cube.addEventListener("click" , () => {
+  input.value = Math.pow(input.value , 3)
+});
+
+
